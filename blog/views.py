f69933f64bc87495fae016c6fb0b170d10a8975b
yangxg <zmrenwu@163.com>
2017-05-01 13:14:54 +0800

    自动生成目录

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 import markdown
 
 from django.shortcuts import render, get_object_or_404
-
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 from comments.forms import CommentForm
 from .models import Post, Category
 
@@ -43,15 +44,16 @@ def detail(request, pk):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        TocExtension(slugify=slugify)
+    ])
+    post.body = md.convert(post.body)
     form = CommentForm()
     comment_list = post.comment_set.all()
     context = {'post': post,
+               'toc': md.toc,
                'form': form,
                'comment_list': comment_list
                }

9e79bcb0182e3f96b517a8a55e9be0288e6d59b0
yangxg <zmrenwu@163.com>
2017-05-09 19:36:36 +0800

    Django 博客统计文章阅读量：http://www.zmrenwu.com/post/26/

--- blog/models.py
+++ blog/models.py
@@ -102,12 +102,19 @@ class Post(models.Model):
     # 和 Category 类似。
     author = models.ForeignKey(User)
 
+    # 统计文章的阅读量
+    views = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         # python_2_unicode_compatible 装饰器用于兼容 Python2
         return self.title
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     class Meta:
         ordering = ['-created_time']
--- blog/views.py
+++ blog/views.py
@@ -193,6 +193,8 @@ def detail(request, pk):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # 阅读量 +1
+    post.increase_views()
     md = markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
